844cc768570c64f3d5b4e8ab66f7d99a17fa1434
Samyam Pradhan <pradhansamyam71@gmail.com>
2026-03-04 19:22:37 +0545

    Test db connection

---
+++ backend/app/database.py
@@ -0,0 +1,19 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ backend/app/test_db.py
@@ -0,0 +1,6 @@
+from .database import engine
+try:
+    conn = engine.connect()
+    print("Database connected successfully!")
+except Exception as e:
+    print("Connection failed:", e)
